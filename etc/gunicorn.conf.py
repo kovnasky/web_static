da80c5c7203cf45a768b5890caec6fbc8c820d1d
kovnasky <kovnatsky@1337.no>
2016-05-20 22:38:59 +0300

    initial commit

--- etc/gunicorn.conf.py
+++
@@ -1,8 +0,0 @@
-import multiprocessing
-
-bind = "unix:/home/box/web/etc/gunicorn.sock"
-workers = 3
-errorlog = '/home/box/web/logs/gunicorn.error.log'
-#accesslog = '/home/user/logs/gunicorn.access.log'
-#loglevel = 'debug'
-proc_name = 'gunicorn_appname'
--- init.sh
+++ init.sh
@@ -1,9 +1,16 @@
+#sudo rm /etc/nginx/sites-enabled/default
+#sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+#sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/ask
+#sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
+#cd /home/box/web/
+#gunicorn -b 0.0.0.0:8080 -D hello:app
+#cd /home/box/web/ask/
+#gunicorn ask.wsgi:application --bind 0.0.0.0:8000 -D
+#sudo /etc/init.d/nginx restart
+
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/ask
-sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
-cd /home/box/web/
-gunicorn -b 0.0.0.0:8080 -D hello:app
-cd /home/box/web/ask/
-gunicorn ask.wsgi:application --bind 0.0.0.0:8000 -D
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test
 sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/hello.conf /etc/gunicorn.d/hello
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/ask
+sudo /etc/init.d/gunicorn restart

6181cae32643947a92cfc5877c571db4ce5f2ade
kovnasky <kovnatsky@1337.no>
2016-05-20 21:20:58 +0300

    initial commit

--- init.sh
+++ init.sh
@@ -1,5 +1,9 @@
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/ask
-sudo /etc/init.d/gunicorn restart
+sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
+cd /home/box/web/
+gunicorn -b 0.0.0.0:8080 -D hello:app
+cd /home/box/web/ask/
+gunicorn ask.wsgi:application --bind 0.0.0.0:8000 -D
+sudo /etc/init.d/nginx restart
